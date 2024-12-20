5735c329411686505486a553c8ed69c82ce8b5e3
rosedskim <rosedskim@naver.com>
2024-12-20 15:39:42 +0900

    pm2 실행

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
-const history = require('connect-history-api-fallback');
-const secure = require('ssl-express-www');
+const express = require("express");
+const path = require("path");
+const serveStatic = require("serve-static");
+const history = require("connect-history-api-fallback");
+const secure = require("ssl-express-www");
 
 let app = express();
 
@@ -10,10 +10,10 @@ app.use(secure);
 app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log('Listening on port ' + port)
+  console.log("Listening on port " + port);
 });
 
-app.on('SIGINT', () => process.exit(1));
+app.on("SIGINT", () => process.exit(1));
